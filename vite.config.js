48de170b9419ac923802547fd5340531431c6b2b
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-03-14 22:35:44 -0300

    Deploy settings

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/fundamentosReactTP3.10/',  // nome do repositório no github
   plugins: [react()],
   server: {
     host: '0.0.0.0',
